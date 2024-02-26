05eb5cea366fda0c2f3f7e04e02cc0e2d6064911
George Ogden <george-ogden@outlook.com>
2024-02-26 15:40:30 +0000

    test trim method

--- src/image_test.py
+++ src/image_test.py
@@ -1,4 +1,6 @@
-from .image import CharacterSegment, Image, LineSegment
+import numpy as np
+
+from .image import CharacterSegment, Image, LineSegment, Segment
 
 
 def test_detect_lines(test_bw_image: Image):
@@ -25,3 +27,26 @@ def test_characters(test_bw_image: Image):
         assert isinstance(character, CharacterSegment)
         assert character in lines[0]
         assert character in test_bw_image
+
+
+def test_trim(test_bw_image: Image):
+    segment = Segment(test_bw_image.image, test_bw_image)
+    trimmed = segment.trim()
+
+    # Check that the trimmed segment is smaller than the original.
+    assert 0 < trimmed.h < segment.h
+    assert 0 < trimmed.w < segment.w
+    assert trimmed.y1 > segment.y1
+    assert trimmed.y2 < segment.y2
+    assert trimmed.x1 > segment.x1
+    assert trimmed.x2 < segment.x2
+
+    # Check that the trimmed segment is still a segment of the original image.
+    assert trimmed in test_bw_image
+    assert trimmed in segment
+
+    # Check that there is no whitespace in the trimmed segment.
+    assert np.any(trimmed.image[0])
+    assert np.any(trimmed.image[-1])
+    assert np.any(trimmed.image[:, 0])
+    assert np.any(trimmed.image[:, -1])
