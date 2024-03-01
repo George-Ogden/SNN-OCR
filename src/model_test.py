2fbd75728b58e45391dbf10b3a1175639ed12b15
George Ogden <george-ogden@outlook.com>
2024-03-01 21:18:01 +0000

    test different architecture

--- src/model_test.py
+++ src/model_test.py
@@ -6,10 +6,10 @@ from .model import SpikingNetwork
 
 @pytest.fixture
 def model() -> SpikingNetwork:
-    return SpikingNetwork(10, 2)
+    return SpikingNetwork((32, 48), 2)
 
 
 def test_forward(model: SpikingNetwork):
-    x = torch.rand(5, 10)
+    x = torch.rand(6, 1, 32, 48)
     output_spk, _ = model(x)
-    assert output_spk.shape == (SpikingNetwork.num_steps, 5, 2)
+    assert output_spk.shape == (SpikingNetwork.num_steps, 6, 2)
