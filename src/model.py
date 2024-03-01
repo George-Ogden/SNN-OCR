d3a77ed21f9cee06b0d9c9d7d500d56e25a76224
George Ogden <george-ogden@outlook.com>
2024-03-01 21:17:50 +0000

    implement CNN

--- src/model.py
+++ src/model.py
@@ -1,5 +1,7 @@
+from typing import Tuple
+
 import snntorch as snn
-import torch
+import torch as th
 import torch.nn as nn
 
 
@@ -7,17 +9,28 @@ import torch.nn as nn
 class SpikingNetwork(nn.Module):
     num_steps = 50
     beta = 0.95
-    num_hidden = 1024
 
-    def __init__(self, num_inputs: int, num_outputs: int):
+    def __init__(self, input_size: Tuple[int, int], num_outputs: int):
         super().__init__()
 
         # Initialize layers
-        self.num_inputs = num_inputs
+        self.input_size = input_size
         self.num_outputs = num_outputs
-        self.fc1 = nn.Linear(self.num_inputs, self.num_hidden)
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 32, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(),
+        )
+
+        with th.no_grad():
+            hidden_size = self.conv(th.zeros(1, 1, *input_size)).shape[-1]
+
         self.lif1 = snn.Leaky(beta=self.beta)
-        self.fc2 = nn.Linear(self.num_hidden, self.num_outputs)
+        self.fc = nn.Linear(hidden_size, num_outputs)
         self.lif2 = snn.Leaky(beta=self.beta)
 
     def forward(self, x):
@@ -30,11 +43,11 @@ class SpikingNetwork(nn.Module):
         mem2_rec = []
 
         for step in range(self.num_steps):
-            cur1 = self.fc1(x)
+            cur1 = self.conv(x)
             spk1, mem1 = self.lif1(cur1, mem1)
-            cur2 = self.fc2(spk1)
+            cur2 = self.fc(spk1)
             spk2, mem2 = self.lif2(cur2, mem2)
             spk2_rec.append(spk2)
             mem2_rec.append(mem2)
 
-        return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
+        return th.stack(spk2_rec, dim=0), th.stack(mem2_rec, dim=0)
