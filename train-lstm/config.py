1ec3241272f780d9aa00504341e0d1e5faca7806
George Ogden <george-ogden@outlook.com>
2024-03-04 10:33:11 +0000

    load data into dataloaders

--- train-lstm/config.py
+++ train-lstm/config.py
@@ -1,1 +1,4 @@
 data_root = "training_data"
+validation_size = 131072
+batch_size = 1024
+sequence_length = 128
--- train-lstm/train.py
+++ train-lstm/train.py
@@ -1,13 +1,34 @@
+import itertools
 import os
 
+import torch as th
+import torch.utils.data as data_utils
 from torchdata.datapipes.iter import FileLister, FileOpener
 
-from config import data_root
+from config import batch_size, data_root, validation_size
+
+th.manual_seed(0)
 
 data_path = os.path.join(os.path.dirname(__file__), data_root)
 dp = FileLister(root=data_path).filter(lambda fname: fname.endswith(".txt"))
 dp = FileOpener(dp)
 
-for line in dp.readlines(strip_newline=False, return_path=False).shuffle().sharding_filter():
-    print(line)
+dp = dp.readlines(strip_newline=True, return_path=False).shuffle().sharding_filter()
+
+val_dataset = list(itertools.islice(dp, validation_size))
+val_dataloader = data_utils.DataLoader(
+    val_dataset,
+    batch_size=batch_size,
+)
+train_dataloader = data_utils.DataLoader(
+    dp,
+    batch_size=batch_size,
+)
+
+for batch in val_dataloader:
+    print(batch)
+    break
+
+for batch in train_dataloader:
+    print(batch)
     break
