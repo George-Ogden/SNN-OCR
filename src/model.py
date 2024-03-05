7c8274dbd98adecc8d6d0d7bda5c302737c5d030
George Ogden <george-ogden@outlook.com>
2024-03-05 08:32:27 +0000

    define LSTM architecture

--- src/model.py
+++ src/model.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Optional, Tuple
 
 import snntorch as snn
 import torch as th
@@ -51,3 +51,34 @@ class SpikingNetwork(nn.Module):
             mem2_rec.append(mem2)
 
         return th.stack(spk2_rec, dim=0), th.stack(mem2_rec, dim=0)
+
+
+# LSTM
+class LSTM(nn.Module):
+    hidden_size = 1024
+    num_layers = 3
+
+    def __init__(self, vocab_size: int):
+        super(LSTM, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Embedding(vocab_size, self.hidden_size),
+            nn.Linear(self.hidden_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size),
+        )
+        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, self.num_layers, batch_first=True)
+        self.decoder = nn.Linear(self.hidden_size, vocab_size)
+        self.h0 = nn.Parameter(th.zeros(self.num_layers, 1, self.hidden_size))
+        self.c0 = nn.Parameter(th.zeros(self.num_layers, 1, self.hidden_size))
+
+    def forward(
+        self, x: th.Tensor, hidden_state: Optional[Tuple[th.Tensor, th.Tensor]] = None
+    ) -> Tuple[th.Tensor, th.Tensor]:
+        if hidden_state is None:
+            hidden_state = th.tile(self.h0, (1, x.shape[0], 1)), th.tile(
+                self.c0, (1, x.shape[0], 1)
+            )
+        x = self.encoder(x)
+        x, hidden_state = self.lstm(x, hidden_state)
+        x = self.decoder(x)
+        return x, hidden_state
