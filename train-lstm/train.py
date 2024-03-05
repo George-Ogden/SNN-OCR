424245ec1c21bfc8cb1500dcc436588a90d6aa9e
George Ogden <george-ogden@outlook.com>
2024-03-05 08:19:28 +0000

    preprocess data

--- train-lstm/train.py
+++ train-lstm/train.py
@@ -1,13 +1,31 @@
 import itertools
 import os
+from typing import List
 
 import torch as th
 import torch.utils.data as data_utils
+import torchtext.transforms as T
 from torchdata.datapipes.iter import FileLister, FileOpener
+from torchvision.transforms import v2
 
-from config import batch_size, data_root, validation_size
+from config import batch_size, data_root, sequence_length, validation_size
 
 th.manual_seed(0)
+
+num_classes = 128 + 1  # ASCII + 1 for padding
+
+
+def tokenizer(string: str) -> List[int]:
+    return [ord(c) for c in string if ord(c) < num_classes - 1]
+
+
+transforms = v2.Compose(
+    [
+        v2.Lambda(tokenizer),
+        T.Truncate(sequence_length),
+        T.ToTensor(num_classes),
+    ]
+)
 
 data_path = os.path.join(os.path.dirname(__file__), data_root)
 dp = FileLister(root=data_path).filter(lambda fname: fname.endswith(".txt"))
@@ -16,6 +34,7 @@ dp = FileOpener(dp)
 dp = dp.readlines(strip_newline=True, return_path=False).shuffle().sharding_filter()
 
 val_dataset = list(itertools.islice(dp, validation_size))
+
 val_dataloader = data_utils.DataLoader(
     val_dataset,
     batch_size=batch_size,
@@ -27,6 +46,7 @@ train_dataloader = data_utils.DataLoader(
 
 for batch in val_dataloader:
     print(batch)
+    print(transforms(batch).shape)
     break
 
 for batch in train_dataloader:
