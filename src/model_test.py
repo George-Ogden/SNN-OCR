f66904c33c9304cf6b9e059bb81121f6a21bddf7
George Ogden <george-ogden@outlook.com>
2024-03-05 08:32:36 +0000

    test LSTM forward

--- src/model_test.py
+++ src/model_test.py
@@ -1,15 +1,35 @@
 import pytest
-import torch
+import torch as th
 
-from .model import SpikingNetwork
+from .model import LSTM, SpikingNetwork
 
 
 @pytest.fixture
-def model() -> SpikingNetwork:
+def snn_model() -> SpikingNetwork:
     return SpikingNetwork((32, 48), 2)
 
 
-def test_forward(model: SpikingNetwork):
-    x = torch.rand(6, 1, 32, 48)
-    output_spk, _ = model(x)
+@pytest.fixture
+def model() -> LSTM:
+    return LSTM(48)
+
+
+def test_snn_forward(snn_model: SpikingNetwork):
+    x = th.rand(6, 1, 32, 48)
+    output_spk, _ = snn_model(x)
     assert output_spk.shape == (SpikingNetwork.num_steps, 6, 2)
+
+
+def test_lstm_seq_forward(model: LSTM):
+    x = th.randint(0, 48, size=(6, 32))
+    output, hidden = model(x)
+    assert output.shape == (6, 32, 48)
+
+
+def test_lstm_step_forward(model: LSTM):
+    x = th.randint(0, 48, size=(1, 32))
+    hidden = model.c0, model.h0
+    for i in range(6):
+        output, hidden = model(x, hidden)
+        x = th.argmax(output, dim=-1)
+    assert x.shape == (1, 32)
