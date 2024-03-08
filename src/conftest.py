622038c4e836571e3160d42082b8141ca4c3893e
George Ogden <george-ogden@outlook.com>
2024-03-08 11:12:18 +0000

    test with new image

--- src/conftest.py
+++ src/conftest.py
@@ -11,3 +11,13 @@ def test_image() -> Image:
 @pytest.fixture
 def test_bw_image(test_image: Image) -> Image:
     return Image(test_image.image < 128)
+
+
+@pytest.fixture
+def test_complex_image() -> Image:
+    return Image.load("images/test.png")
+
+
+@pytest.fixture
+def test_complex_bw_image(test_complex_image: Image) -> Image:
+    return Image(test_complex_image.image < 128)
