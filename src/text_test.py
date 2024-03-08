6a9753bdb744edfabacc9cd5f657876b6189c741
George Ogden <george-ogden@outlook.com>
2024-03-08 10:49:33 +0000

    test block on single page

--- src/text_test.py
+++ src/text_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from .image import Image
-from .text import LineText
+from .text import Block, LineText
 
 
 def test_linetext(test_bw_image: Image):
@@ -21,3 +21,22 @@ def test_linetext(test_bw_image: Image):
     # Check character spacing is approximately correct.
     for char in text.stream[1:]:
         assert 0 <= char.spacing.h < 1.2
+
+
+def test_single_block(test_bw_image: Image):
+    lines = test_bw_image.detect_lines()
+    blocks = Block.from_lines(lines)
+
+    # Check that the block contains all the lines.
+    assert len(blocks) == 1
+    (block,) = blocks
+    assert len(block.stream) == np.sum([len(line.detect_characters()) for line in lines])
+    assert np.count_nonzero([char.spacing.v is not None for char in block.stream]) == len(lines) - 1
+
+    # Check other spacing properties.
+    assert np.allclose(block.spacing, 15, atol=3)
+    assert np.all([char.spacing.h is not None for char in block.stream])
+
+    # Check that the block is positioned correctly.
+    assert lines[0].x1 - 5 <= block.x1 <= lines[0].x1
+    assert block.y1 == lines[0].y1
